e73c4122a79f4d09e331adfedefa753b89772557
robertocannella <robertocannella@gmail.com>
2021-10-25 22:20:42 -0400

    added binaryCount() to EasyMath class.

--- src/com/robertocannella/Main.java
+++ src/com/robertocannella/Main.java
@@ -1,10 +1,33 @@
 package com.robertocannella;
 
+import java.util.ArrayList;
+import java.util.EmptyStackException;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        var game = new EasyMath(0,2);
-        var result = game.foursGame(80);
+
+        List<Integer> list = new ArrayList<>();
+
+     //   80 X 5 400
+     //   20 X 2 40
+        // 888 X 5 4440
+        // 60 X 74 = 4440
+  // 63 X 7054673721340388 = 444444444444444444
+  // 62 X 7168458781362 = 444444444444444
+
+ //       list.add(80);
+  //      list.add(62);
+ //      list.add(67);
+//        list.add(90306);
+        list.add(60);
+//       list.add(81);
+ //       list.add(888);
+ //       list.add(80);
+
+        for(int num : list)
+            System.out.println(EasyMath.binaryCounter(200,num));
 
     }
 }
